a70b43d3cde5f086d6ec2aa804455087ad22aec3
Digitechz <chandranilom@gmail.com>
2026-02-02 17:19:31 +0530

    again Edited viewstats command

--- commands/viewstats.js
+++ commands/viewstats.js
@@ -1,24 +1,13 @@
-const { renderStats } = require("../stats");
-
-// Optional: allow everyone to view stats
-// You can later restrict certain commands with allowedUsers array
+const { initUser, renderStats } = require("../stats");
 
 module.exports = {
   name: "viewstats",
-  description: "View your own or another user's stats",
+  description: "View stats for a character",
   execute(message, args) {
-    // Default to self if no argument provided
-    const charName = args[0] || message.author.username;
+    const charName = args[0] || message.author.username; // default to author's name
+    initUser(charName); // make sure the character exists
 
-    // Render the stats using your existing renderStats function
-    let stats;
-    try {
-      stats = renderStats(charName);
-    } catch (err) {
-      console.error(err);
-      return message.reply("❌ Could not find stats for that character.");
-    }
-
-    message.channel.send(`**Stats for ${charName}:**\n${stats}`);
-  }
+    const statsOutput = renderStats(charName);
+    message.channel.send(statsOutput);
+  },
 };
