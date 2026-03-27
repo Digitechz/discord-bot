4d06d9d9d10561f6fcbb4856b45929d3235d7576
Digitechz <chandranilom@gmail.com>
2026-03-27 12:16:25 +0530

    update steph to gemini

--- commands/steph.js
+++ commands/steph.js
@@ -11,7 +11,7 @@ module.exports = {
       }
 
       const res = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GOOGLE_API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GOOGLE_API_KEY}`,
         {
           method: "POST",
           headers: {
